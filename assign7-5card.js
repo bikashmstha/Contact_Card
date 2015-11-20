a7ede1bcaa65f757fc185ab3801aca632f32d373
Anna Propas <apropas@gmail.com>
2015-11-20 00:59:33 -0800

    added jQuery functionality for cards

--- assign7-5card.js
+++ assign7-5card.js
@@ -1,15 +1,27 @@
 $(document).ready(function(){
 
+	
   $('.submit_btn').hover(function(){
     $(this).css('background-color', '#EA833A');
     }, 
     function() {
     $(this).css('background-color', '#CF6113');
   });
-
-  
-
 });
+
+	$(document).on('click', '.submit_btn', function(){
+	  var firstname = $('form').find(('.first_name')).val();
+	  var lastname = $('form').find(('.last_name')).val();
+	  var desc = $('form').find(('.desc')).val();
+
+	  $('#cards').append('<div class="acard">' + firstname + ' ' + 
+	  	lastname + '<p>Click for Description</p>' + '</div>');
+
+	  
+	 });
+
+
+
 
 
 
