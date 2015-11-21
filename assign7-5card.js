a7e65af9897268c8fffae1f2bac1cc87aef3393d
Anna Propas <apropas@gmail.com>
2015-11-20 17:32:46 -0800

    finished jQuery functionality for cards

--- assign7-5card.js
+++ assign7-5card.js
@@ -1,6 +1,33 @@
-$(document).ready(function(){
 
-	
+$(document).on('click', '.submit_btn', function(){
+  var firstname = $('.first_name').val();
+  var lastname = $('.last_name').val();
+  var name = firstname + ' ' + lastname;
+  
+  $('#cards').append('<div class="acard">' + name + '<p>Click for Description</p>' + '</div>');
+
+});
+
+$(document).on('click', '.acard', function(){
+	var firstname = $('.first_name').val();
+	var lastname = $('.last_name').val();
+	var name = firstname + ' ' + lastname;
+	var desc = $('.desc').val();
+
+
+	if ($(this).html() == name + '<p>Click for Description</p>') 
+	{ 
+	 $(this).html(desc); 
+	} 
+	else 
+	{ 
+	 $(this).html(name + '<p>Click for Description</p>'); 
+	};
+  
+});
+
+
+$(document).ready(function(){
   $('.submit_btn').hover(function(){
     $(this).css('background-color', '#EA833A');
     }, 
@@ -8,20 +35,5 @@ $(document).ready(function(){
     $(this).css('background-color', '#CF6113');
   });
 });
-
-	$(document).on('click', '.submit_btn', function(){
-	  var firstname = $('form').find(('.first_name')).val();
-	  var lastname = $('form').find(('.last_name')).val();
-	  var desc = $('form').find(('.desc')).val();
-
-	  $('#cards').append('<div class="acard">' + firstname + ' ' + 
-	  	lastname + '<p>Click for Description</p>' + '</div>');
-
-	  
-	 });
-
-
-
-
 
 
